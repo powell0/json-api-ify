c57a7d3228e7a06a3de3e97298845b3a7788e382
Ian Ferguson <iferguson@ddswireless.com>
2017-06-22 15:15:02 -0700

    Added option to omit full linkage of related resources

--- test/complex.test.js
+++ test/complex.test.js
@@ -86,7 +86,7 @@ describe('complex tests', function() {
         }, done);
     });
 
-    context('super nested', function() {
+    context('super nested (full linkage)', function() {
         let dataset = [
             {
                 _id: '54735750e16638ba1eee59cb',
@@ -156,6 +156,98 @@ describe('complex tests', function() {
         let error, payload;
         before(function(done) {
             serializer.serialize('libraries', dataset, function(e, p) {
+                error = e;
+                payload = p;
+                done(e);
+            });
+        });
+
+        it('should not throw an error', function() {
+            expect(error).to.not.exist;
+        });
+
+        it('should include the correct related resources', function() {
+            let types = _(payload.included).groupBy('type').mapValues(function(docs) {
+                return docs.length;
+            }).value();
+            expect(types).to.have.property('books', 3);
+            expect(types).to.have.property('libraries', 2);
+            expect(types).to.have.property('authors', 2);
+            expect(types).to.have.property('cities', 1);
+            expect(types).to.have.property('states', 1);
+        });
+    });
+
+    context('super nested (omit full linkage)', function() {
+        let dataset = [
+            {
+                _id: '54735750e16638ba1eee59cb',
+                name: 'Lone Tree Public Library',
+                address: {
+                    street: '293 S. 1st St',
+                    city: {
+                        id: 10,
+                        name: 'Denver',
+                        state: 36
+                    },
+                    state: {
+                        id: 36,
+                        name: 'Colorado',
+                        latitude: -38.23097398723987,
+                        longitude: 101.234972349872398,
+                        capital: 'Denver',
+                        libraries: [
+                            '54735750e16638ba1eee59cb',
+                            '54735750e16638ba1eee59dd',
+                            {
+                                _id: '54735750e16638ba1eee59ac',
+                                name: 'Denver Public Library',
+                                address: {
+                                    street: '1001 S. Broadway',
+                                    state: 36
+                                }
+                            }
+                        ]
+                    }
+                },
+                books: [
+                    {
+                        _id: '52735730e16632ba1eee62dd',
+                        title: 'Tesla, SpaceX, and the Quest for a Fantastic Future',
+                        isbn: '978-0062301239',
+                        author: {
+                            _id: '2934f384bb824a7cb7b238b8dc194a22',
+                            firstName: 'Ashlee',
+                            lastName: 'Vance',
+                            books: [
+                                {
+                                    _id: '52735730e16632ba1eee62dd',
+                                    title: 'Tesla, SpaceX, and the Quest for a Fantastic Future',
+                                    isbn: '978-0062301239'
+                                },
+                                {
+                                    _id: '52735730e16632ba1eee62ce'
+                                }
+                            ]
+                        }
+                    },
+                    {
+                        _id: '52735780e16610ba1eee15cd',
+                        title: 'Steve Jobs',
+                        isbn: '978-1451648546',
+                        author: {
+                            _id: '5ed95269a8334d8a970a2bd9fa599288',
+                            firstName: 'Walter',
+                            lastName: 'Isaacson'
+                        }
+                    }
+                ]
+            }
+        ];
+
+        let error, payload;
+        before(function(done) {
+            serializer.serialize('libraries', dataset, {omitFullLinkage: true}, function(e, p) {
                 error = e;
                 payload = p;
                 done(e);
